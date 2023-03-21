2ab0a093b1c09dedebe2d914a7369cfc2fc860c1
Paul Daniël <vanschevikhoven.pd@gmail.com>
2023-03-21 13:54:30 +0100

    first commit

---
+++ src/pages/EventsPage.jsx
@@ -0,0 +1,81 @@
+import { Box, Heading, Image, SimpleGrid } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+
+const API_URL = "http://localhost:3000";
+
+export const EventsPage = () => {
+  const [events, setEvents] = useState([]);
+  const [categories, setCategories] = useState({});
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const response = await fetch(`${API_URL}/events`);
+      const data = await response.json();
+      setEvents(data);
+    };
+
+    const fetchCategories = async () => {
+      const response = await fetch(`${API_URL}/categories`);
+      const data = await response.json();
+      const categoriesObject = {};
+      data.forEach((category) => {
+        categoriesObject[category.id] = category.name;
+      });
+      setCategories(categoriesObject);
+    };
+
+    fetchEvents();
+    fetchCategories();
+  }, []);
+
+  return (
+    <>
+      <Heading as="h1">Events</Heading>
+
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3 }}
+        spacing={5}
+        justifyItems="center"
+      >
+        {events.map((event) => (
+          <Box
+            key={event.id}
+            p="6"
+            borderWidth="1px"
+            borderRadius="lg"
+            overflow="hidden"
+            boxSize="md"
+            mt="50px"
+            _hover={{ boxShadow: "lg" }}
+          >
+            <Image
+              src={event.image}
+              alt={event.title}
+              width="400px"
+              height="250px"
+              borderWidth="1px"
+              borderRadius="lg"
+            />
+
+            <Box mt="20px">
+              <Box as="h4" fontWeight="semibold" isTruncated>
+                {event.title}
+              </Box>
+              <Box>{event.description}</Box>
+              <Box>
+                Categories:{" "}
+                {event.categoryIds
+                  .map((categoryId) => categories[categoryId])
+                  .join(", ")}
+              </Box>
+              <Box>
+                Start Time: {new Date(event.startTime).toLocaleString()}
+              </Box>
+              <Box>End Time: {new Date(event.endTime).toLocaleString()}</Box>
+            </Box>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </>
+  );
+};
